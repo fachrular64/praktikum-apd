5e92f0a6313b646e49b4264561f9591f07cd54b0
fachrular64 <fachrul.aulia2006@gmail.com>
2025-10-09 00:24:31 +0800

    post-test keempat

--- kelas/post-test/post-test-apd-4/2509106030-Fachrul Aulia Rahman-PT-4.py
+++ kelas/post-test/post-test-apd-4/2509106030-Fachrul Aulia Rahman-PT-4.py
@@ -10,34 +10,39 @@ for i in range(kesempatan, 0, -1):
     password = input("Masukkan password: ")
 
     if username == username_benar and password == password_benar:
-        print("Selamat datang,",username_benar,"!")
+        print("Selamat datang,", username_benar, "!")
         login_berhasil = True 
         break
-    else:
+    else: 
         print("Login gagal. Sisa percobaan:", i - 1)
         if i - 1 == 0:
-            print("Akun terkunci!")
+            print("Program dihentikan!")
 
 if login_berhasil:
-    print("=== Program Menentukan Jenis Segitiga ===")
-    sisi1 = int(input("Masukkan sisi pertama: "))
-    sisi2 = int(input("Masukkan sisi kedua: ")) 
-    sisi3 = int(input("Masukkan sisi ketiga: "))
+    lanjut = "ya"
+    while lanjut == "ya":
+        print("=== Program Menentukan Jenis Segitiga ===")
+        sisi1 = int(input("Masukkan sisi pertama: "))
+        sisi2 = int(input("Masukkan sisi kedua: ")) 
+        sisi3 = int(input("Masukkan sisi ketiga: "))
 
-    if (sisi1 + sisi2 > sisi3) and (sisi1 + sisi3 > sisi2) and (sisi2 + sisi3 > sisi1):
-        if sisi1 == sisi2 == sisi3:
-            jenis = "Segitiga Sama Sisi"
-        elif sisi1 == sisi2 or sisi2 == sisi3 or sisi1 == sisi3:
-            jenis = "Segitiga Sama Kaki"
+        if (sisi1 + sisi2 > sisi3) and (sisi1 + sisi3 > sisi2) and (sisi2 + sisi3 > sisi1):
+            if sisi1 == sisi2 == sisi3:
+                jenis = "Segitiga Sama Sisi"
+            elif sisi1 == sisi2 or sisi2 == sisi3 or sisi1 == sisi3:
+                jenis = "Segitiga Sama Kaki"
+            else:
+                jenis = "Segitiga Sembarang"
+
+            print("Jenis segitiga:", jenis)
+
+            s = (sisi1 + sisi2 + sisi3) / 2
+            luas = math.sqrt(s * (s - sisi1) * (s - sisi2) * (s - sisi3))
+            print("Luas segitiga =", luas)
         else:
-            jenis = "Segitiga Sembarang"
+            print("Bukan segitiga")
 
-        print("Jenis segitiga: ", jenis)
+        print("==========================================")
+        lanjut = input("Apakah ingin melanjutkan program? (ya/tidak): ")
 
-        s = (sisi1 + sisi2 + sisi3) / 2
-        luas = math.sqrt(s * (s - sisi1) * (s - sisi2) * (s - sisi3))
-        print("Luas segitiga = ", luas)
-    else:
-        print("Bukan segitiga")
-
-    print("==========================================")
+    print("Program selesai. Terima kasih!")
